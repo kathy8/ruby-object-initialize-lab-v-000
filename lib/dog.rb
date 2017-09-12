8f81db32bd283f8cc3ad5dcb99dcafd360eca85d
Katherine Gruenberg <katherine.gruenberg@gmail.com>
2017-09-12 16:53:48 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,19 @@
 class Dog
-  def initialize
+  def initialize(name, breed = "Mutt")
+    @name = name
+    @breed = breed
+  end
+
+  def name = (fido)
+    @name = fido
+    fido.name = "fido"
+  end
+
+  def breed=(fido)
+    if @breed = "Pug"
+      return "Pug"
+    else
+      return "Mutt"
+    end
+  end
+end
